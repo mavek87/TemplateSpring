1786bd46f0adafa41ec6a1029f5521059d9899e4
matteo <matteo.veroni@giuffre.it>
2024-09-11 11:42:47 +0200

    add tests for spring profiles

--- src/test/java/com/matteoveroni/templatespring/TemplateSpringApplicationTests.java
+++ src/test/java/com/matteoveroni/templatespring/TemplateSpringApplicationTests.java
@@ -1,13 +1,43 @@
 package com.matteoveroni.templatespring;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TemplateSpringApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Nested
+    @ActiveProfiles("dev")
+    class DevProfileTests {
+
+        @Autowired
+        private Environment environment;
+
+        @Test
+        void testDevProfile() {
+            String[] activeProfiles = environment.getActiveProfiles();
+            assertThat(activeProfiles).containsExactly("dev");
+        }
+    }
+
+    @Nested
+    @ActiveProfiles("prod")
+    class ProdProfileTests {
+
+        @Autowired
+        private Environment environment;
+
+        @Test
+        void testProdProfile() {
+            String[] activeProfiles = environment.getActiveProfiles();
+            assertThat(activeProfiles).containsExactly("prod");
+        }
     }
 
 }

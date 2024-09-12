b58316380605c79b1078b549ca47a101d28cb27f
mavek87 <matver87@gmail.com>
2024-09-12 19:51:03 +0200

    add count in ResponseListDTO

--- src/main/java/com/matteoveroni/templatespring/domain/dto/ResponseListDTO.java
+++ src/main/java/com/matteoveroni/templatespring/domain/dto/ResponseListDTO.java
@@ -3,5 +3,11 @@ package com.matteoveroni.templatespring.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
-public record ResponseListDTO<T>(@JsonProperty("results") List<T> results) {
+public record ResponseListDTO<T>(
+        @JsonProperty("results") List<T> results,
+        @JsonProperty("count") Integer count
+) {
+    public ResponseListDTO(List<T> results) {
+        this(results, results != null ? results.size() : 0);
+    }
 }

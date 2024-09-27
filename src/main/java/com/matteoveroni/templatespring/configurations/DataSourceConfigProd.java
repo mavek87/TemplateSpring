050f93e00cf00507dc3b6f4914fa0c65618a3bdf
mavek87 <matver87@gmail.com>
2024-09-27 13:12:49 +0200

    rename

--- src/main/java/com/matteoveroni/templatespring/configurations/DataSourceConfigProd.java
+++ src/main/java/com/matteoveroni/templatespring/configurations/DataSourceConfigProd.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("!dev")
-public class DatabaseConfigProd {
+public class DataSourceConfigProd {
 
     @Primary
     @Bean

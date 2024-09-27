9faf1b42484624131351740e4e0c479d6f706f4a
mavek87 <matver87@gmail.com>
2024-09-27 13:55:07 +0200

    keycloack db port 5433 and app db port 5432

--- src/main/java/com/matteoveroni/templatespring/configurations/DataSourceConfigProd.java
+++ src/main/java/com/matteoveroni/templatespring/configurations/DataSourceConfigProd.java
@@ -16,7 +16,7 @@ public class DataSourceConfigProd {
     public DataSource getDataSource() {
         return DataSourceBuilder.create()
                 .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://localhost:5433/app_db")
+                .url("jdbc:postgresql://localhost:5432/app_db")
                 .username("postgres")
                 .password("password")
                 .build();
